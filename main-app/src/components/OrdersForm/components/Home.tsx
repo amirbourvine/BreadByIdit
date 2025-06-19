97544414c0363790c477c49feeb358763facc1ac
amirbourvine <amirbourvine@gmail.com>
2025-06-20 01:27:00 +0300

    same

--- main-app/src/components/OrdersForm/components/Home.tsx
+++ main-app/src/components/OrdersForm/components/Home.tsx
@@ -1,7 +1,16 @@
 function Home() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <h1 className="text-center">Welcome to the Home Page</h1>
+        <div className="flex items-center justify-center" style={{ 
+            height: 'calc(100vh - 160px)', 
+            width: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            margin: 0
+        }}>
+            <h1 className="text-center text-2xl font-semibold">Welcome to the Home Page</h1>
         </div>
     )
 }
