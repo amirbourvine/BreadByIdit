3a0f98ce7b306cf67ce2503d407a32a85c5e7c77
amirbourvine <amirbourvine@gmail.com>
2025-06-20 01:28:43 +0300

    return to good old

--- main-app/src/components/OrdersForm/components/Home.tsx
+++ main-app/src/components/OrdersForm/components/Home.tsx
@@ -1,17 +1,8 @@
 function Home() {
     return (
-        <div className="flex items-center justify-center" style={{ 
-            height: 'calc(100vh - 160px)', 
-            width: '100%',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            margin: 0
-        }}>
-            <h1 className="text-center text-2xl font-semibold">Welcome to the Home Page</h1>
-        </div>
+    <div>
+        <h1>Welcome to the Home Page</h1>
+    </div>
     )
 }
 
