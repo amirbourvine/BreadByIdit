cd9a0c1bc7357d0c6b5604085da8a1d189e484f6
amirbourvine <amirbourvine@gmail.com>
2025-06-20 01:24:33 +0300

    same

--- main-app/src/components/OrdersForm/components/Home.tsx
+++ main-app/src/components/OrdersForm/components/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <h1>Welcome to the Home Page</h1>
+            <h1 className="text-center">Welcome to the Home Page</h1>
         </div>
     )
 }
