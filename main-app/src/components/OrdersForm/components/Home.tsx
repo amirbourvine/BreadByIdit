fbeeba941a31df3cce60f76a2b0f604d881819a0
amirbourvine <amirbourvine@gmail.com>
2025-06-23 22:36:19 +0300

    same

--- main-app/src/components/OrdersForm/components/Home.tsx
+++ main-app/src/components/OrdersForm/components/Home.tsx
@@ -83,7 +83,8 @@ const Home = () => {
                   margin: '0 0 8px 0', 
                   fontWeight: 'bold',
                   fontSize: window.innerWidth <= 768 ? '16px' : '18px',
-                  textAlign: 'center'
+                  textAlign: 'center',
+                  direction: /[\u0590-\u05FF]/.test(product.name) ? 'rtl' : 'ltr'
                 }}
               >
                 {product.name}
